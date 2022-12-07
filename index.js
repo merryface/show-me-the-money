3ba5796437b4053c2b78fdea405338e42b42dd2d
Tarik <tarikmerrylees@gmail.com>
2022-12-07 23:13:02 +0000

    guard number below zero

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function salary(base = 0) {
-  if (typeof base != Number) return "enter a valid number"
-  if (typeof base < 0) return "enter an amount above 0"
+  if (typeof base != 'number') return "enter a valid number"
+  if (base < 0) return "enter an amount above 0"
   return 0
 }
 
