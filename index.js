f8ffceaa8d84ad187ce0e2414ca83d83e1245fbd
Tarik <tarikmerrylees@gmail.com>
2022-12-07 23:08:47 +0000

    guard type

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function salary(base) {
-  if (typeof base != Number) {
-    return "enter a valid number"
-  }
+function salary(base = 0) {
+  if (typeof base != Number) return "enter a valid number"
+  if (typeof base < 0) return "enter an amount above 0"
   return 0
 }
 
