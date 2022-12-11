f75cebe0ed53ba77925fdc84afab8c82fe1caf29
Tarik <tarikmerrylees@gmail.com>
2022-12-11 15:04:11 +0000

    reset button too

--- script.js
+++ script.js
@@ -100,8 +100,30 @@ getEl("reset").addEventListener("click", () => {
     'hed',
     'lateHome',
     'tax',
-
+    'table_base',
+    'table_perDiem',
+    'table_cpr',
+    'table_tbp',
+    'table_ned',
+    'table_hed',
+    'table_lateHome',
+    'table_soldDays',
+    'table_tax',
   ]
+
+  const tableFields = [
+    'table_base',
+    'table_perDiem',
+    'table_cpr',
+    'table_tbp',
+    'table_ned',
+    'table_hed',
+    'table_lateHome',
+    'table_soldDays',
+    'table_tax',
+  ]
+  
   getEl('tbpExtra').value = '200'
   inputFields.forEach(el => getEl(el).value = '')
+  tableFields.forEach(el => getEl(el).innerText = '')
 })
