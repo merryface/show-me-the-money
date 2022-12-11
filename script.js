6a4aa0548ce2e67d4139ecfce3a494d8f6317787
Tarik <tarikmerrylees@gmail.com>
2022-12-11 00:02:18 +0000

    implement vacation sell off into app

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import cprCalc from './utils/cprCalc.js';
 import nedCalc from './utils/nedCalc.js'
 import hedCalc from './utils/hedCalc.js'
 import lateHomeCalc from './utils/lateHomeCalc.js'
+import vacationSellOffCalc from './utils/vacationSellOffCalc.js';
 
 const getElValue = id => document.getElementById(id).value
 const getEl = id => document.getElementById(id)
@@ -36,11 +37,12 @@ getEl("calc").addEventListener("click", () => {
   const dutyDays = Number(getElValue('dutyDays'))
   const blockHours = Number(getElValue('blockHours'))
   const tbpExtra = Number(getElValue('tbpExtra'))-200
+  const tbpActive = tbpExtra > 0
   const tbpHours = Number(getElValue('tbpHours'))
   const ned = Number(getElValue('ned'))
   const hed = Number(getElValue('hed'))
   const lateHome = Number(getElValue('lateHome'))
-  // const soldDays = Number(getElValue('soldDays'))
+  const soldDays = Number(getElValue('soldDays'))
   const tax = Number(getElValue('tax'))
   
   const total = salary({
@@ -50,26 +52,30 @@ getEl("calc").addEventListener("click", () => {
     blockHours,
     ccOnboard,
     apuAvail,
-    tbpActive: tbpExtra > 0,
+    tbpActive,
     tbpExtra,
     tbpHours,
     ned,
     hed,
-    lateHome
+    lateHome,
+    soldDays
   })
+
+  let correctedBase = tbpActive ? base * (1 + (0.025 * tbpExtra/4)): base
   
   net = total - (total * (tax/100)).toFixed(2)
 
-  const cpr = cprCalc(blockHours, ccOnboard, apuAvail).toFixed(2)
-  const tbp = tbpExtra > 0 ? (tbpHours * 105).toFixed(2) : 0
+  const cpr = cprCalc(blockHours, ccOnboard, apuAvail)
+  const tbp = tbpExtra > 0 ? (tbpHours * 105): 0
 
-  getEl("table_base").innerText = `${base.toLocaleString("en-US")} €`
+  getEl("table_base").innerText = `${correctedBase.toLocaleString("en-US")} €`
   getEl("table_perDiem").innerText = `${(dutyDays*70).toLocaleString("en-US")} €`
   getEl("table_cpr").innerText = `${cpr.toLocaleString("en-US")} €`
   getEl("table_tbp").innerText = `${tbp.toLocaleString("en-US")} €`
   getEl("table_ned").innerText = `${nedCalc(ned, isCaptain).toLocaleString("en-US")} €`
   getEl("table_hed").innerText = `${hedCalc(hed, isCaptain).toLocaleString("en-US")} €`
   getEl("table_lateHome").innerText = `${lateHomeCalc(lateHome, isCaptain).toLocaleString("en-US")} €`
+  getEl("table_soldDays").innerText = `${vacationSellOffCalc(soldDays, correctedBase).toLocaleString("en-US")} €`
   getEl("table_tax").innerText = `${(-(tax/100)*total).toLocaleString("en-US")} €`
 
   getEl("table_beforeTax").innerText = `${total.toLocaleString("en-US")} €`
--- utils/vacationSellOffCalc.js
+++ utils/vacationSellOffCalc.js
@@ -1,4 +1,4 @@
 export default function vacationSellOffCalc(soldDays, base) {
   // 0.5% base increase per sold vacation day 
-  return base * 0.05 * soldDays
+  return base * 0.005 * soldDays
 }
