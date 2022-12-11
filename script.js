ee6adecab980bcaff462f320dd0b133e1ca0e67b
Tarik <tarikmerrylees@gmail.com>
2022-12-11 15:45:07 +0000

    move calc listener out

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import nedCalc from './utils/nedCalc.js'
 import hedCalc from './utils/hedCalc.js'
 import lateHomeCalc from './utils/lateHomeCalc.js'
 import vacationSellOffCalc from './utils/vacationSellOffCalc.js';
+import calculateListener from './utils/calculateListener.js';
 
 const getElValue = id => document.getElementById(id).value
 const getEl = id => document.getElementById(id)
@@ -33,62 +34,61 @@ const toggleSwitch = (id, state) => {
 const toggleSwitches =["isCaptain", "ccOnboard", "apuAvail"]
 toggleSwitches.forEach(id => getEl(id).addEventListener("click", toggleSwitch(id, true)))
 
-
-getEl("calc").addEventListener("click", () => {
-  const tbpExtra = Number(getElValue('tbpExtra'))-200
-  const tbpActive = tbpExtra > 0
+calculateListener(isCaptain, ccOnboard, apuAvail, net)
+//   const tbpExtra = Number(getElValue('tbpExtra'))-200
+//   const tbpActive = tbpExtra > 0
   
-  const base = Number(getElValue('base'))
-  const dutyDays = Number(getElValue('dutyDays'))
-  const blockHours = Number(getElValue('blockHours'))
-  const tbpHours = Number(getElValue('tbpHours'))
-  const ned = Number(getElValue('ned'))
-  const hed = Number(getElValue('hed'))
-  const lateHome = Number(getElValue('lateHome'))
-  const soldDays = Number(getElValue('soldDays'))
-  const tax = Number(getElValue('tax'))
+//   const base = Number(getElValue('base'))
+//   const dutyDays = Number(getElValue('dutyDays'))
+//   const blockHours = Number(getElValue('blockHours'))
+//   const tbpHours = Number(getElValue('tbpHours'))
+//   const ned = Number(getElValue('ned'))
+//   const hed = Number(getElValue('hed'))
+//   const lateHome = Number(getElValue('lateHome'))
+//   const soldDays = Number(getElValue('soldDays'))
+//   const tax = Number(getElValue('tax'))
   
-  const total = salary({
-    base,
-    isCaptain,
-    dutyDays,
-    blockHours,
-    ccOnboard,
-    apuAvail,
-    tbpActive,
-    tbpExtra,
-    tbpHours,
-    ned,
-    hed,
-    lateHome,
-    soldDays
-  })
+//   const total = salary({
+//     base,
+//     isCaptain,
+//     dutyDays,
+//     blockHours,
+//     ccOnboard,
+//     apuAvail,
+//     tbpActive,
+//     tbpExtra,
+//     tbpHours,
+//     ned,
+//     hed,
+//     lateHome,
+//     soldDays
+//   })
 
-  let correctedBase = tbpActive ? base * (1 + (0.025 * tbpExtra/4)): base
+//   let correctedBase = tbpActive ? base * (1 + (0.025 * tbpExtra/4)): base
   
-  net = total - (total * (tax/100))
+//   net = total - (total * (tax/100))
 
-  const cpr = cprCalc(blockHours, ccOnboard, apuAvail)
-  const tbp = tbpExtra > 0 ? (tbpHours * 105): 0
+//   const cpr = cprCalc(blockHours, ccOnboard, apuAvail)
+//   const tbp = tbpExtra > 0 ? (tbpHours * 105): 0
 
-  const table_cells = [
-    ['table_base', formatCurrency(correctedBase)],
-    ['table_perDiem', formatCurrency((dutyDays*70))],
-    ['table_cpr', formatCurrency(cpr)],
-    ['table_tbp', formatCurrency(tbp)],
-    ['table_ned', formatCurrency(nedCalc(ned, isCaptain))],
-    ['table_hed', formatCurrency(hedCalc(hed, isCaptain))],
-    ['table_lateHome', formatCurrency(lateHomeCalc(lateHome, isCaptain), isCaptain)],
-    ['table_soldDays', formatCurrency(vacationSellOffCalc(soldDays,correctedBase))],
-    ['table_tax', formatCurrency(-(tax/100)*total)],
+//   const table_cells = [
+//     ['table_base', formatCurrency(correctedBase)],
+//     ['table_perDiem', formatCurrency((dutyDays*70))],
+//     ['table_cpr', formatCurrency(cpr)],
+//     ['table_tbp', formatCurrency(tbp)],
+//     ['table_ned', formatCurrency(nedCalc(ned, isCaptain))],
+//     ['table_hed', formatCurrency(hedCalc(hed, isCaptain))],
+//     ['table_lateHome', formatCurrency(lateHomeCalc(lateHome, isCaptain), isCaptain)],
+//     ['table_soldDays', formatCurrency(vacationSellOffCalc(soldDays,correctedBase))],
+//     ['table_tax', formatCurrency(-(tax/100)*total)],
 
-    ['table_beforeTax', total],
-    ['table_afterTax', net],
-  ]
+//     ['table_beforeTax', total],
+//     ['table_afterTax', net],
+//   ]
 
-  table_cells.forEach(cell => getEl(cell[0]).innerText = `${cell[1]}`)
-  getEl("breakdown").scrollIntoView({behavior: "smooth"})
-});
+//   table_cells.forEach(cell => getEl(cell[0]).innerText = `${cell[1]}`)
+//   getEl("breakdown").scrollIntoView({behavior: "smooth"})
+// });
 
 getEl("reset").addEventListener("click", () => {
   const inputFields = [
@@ -122,7 +122,7 @@ getEl("reset").addEventListener("click", () => {
     'table_soldDays',
     'table_tax',
   ]
-  
+
   getEl('tbpExtra').value = '200'
   inputFields.forEach(el => getEl(el).value = '')
   tableFields.forEach(el => getEl(el).innerText = '')
---
+++ utils/calculateListener.js
@@ -0,0 +1,68 @@
+import salary from './salaryCalc.js';
+import cprCalc from './cprCalc.js';
+import nedCalc from './nedCalc.js'
+import hedCalc from './hedCalc.js'
+import lateHomeCalc from './lateHomeCalc.js'
+import vacationSellOffCalc from './vacationSellOffCalc.js';
+const formatCurrency = value => new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(value)
+
+const getEl = id => document.getElementById(id)
+const getElValue = id => document.getElementById(id).value
+
+export default function calculateListener(isCaptain, ccOnboard, apuAvail, net) {
+  getEl("calc").addEventListener("click", () => {
+    const tbpExtra = Number(getElValue('tbpExtra'))-200
+    const tbpActive = tbpExtra > 0
+  
+    const base = Number(getElValue('base'))
+    const dutyDays = Number(getElValue('dutyDays'))
+    const blockHours = Number(getElValue('blockHours'))
+    const tbpHours = Number(getElValue('tbpHours'))
+    const ned = Number(getElValue('ned'))
+    const hed = Number(getElValue('hed'))
+    const lateHome = Number(getElValue('lateHome'))
+    const soldDays = Number(getElValue('soldDays'))
+    const tax = Number(getElValue('tax'))
+  
+    const total = salary({
+      base,
+      isCaptain,
+      dutyDays,
+      blockHours,
+      ccOnboard,
+      apuAvail,
+      tbpActive,
+      tbpExtra,
+      tbpHours,
+      ned,
+      hed,
+      lateHome,
+      soldDays
+    })
+
+    let correctedBase = tbpActive ? base * (1 + (0.025 * tbpExtra/4)): base
+  
+    net = total - (total * (tax/100))
+
+    const cpr = cprCalc(blockHours, ccOnboard, apuAvail)
+    const tbp = tbpExtra > 0 ? (tbpHours * 105): 0
+
+    const table_cells = [
+      ['table_base', formatCurrency(correctedBase)],
+      ['table_perDiem', formatCurrency((dutyDays*70))],
+      ['table_cpr', formatCurrency(cpr)],
+      ['table_tbp', formatCurrency(tbp)],
+      ['table_ned', formatCurrency(nedCalc(ned, isCaptain))],
+      ['table_hed', formatCurrency(hedCalc(hed, isCaptain))],
+      ['table_lateHome', formatCurrency(lateHomeCalc(lateHome, isCaptain), isCaptain)],
+      ['table_soldDays', formatCurrency(vacationSellOffCalc(soldDays,correctedBase))],
+      ['table_tax', formatCurrency(-(tax/100)*total)],
+      ['table_soldDays', formatCurrency(vacationSellOffCalc(soldDays,correctedBase))],
+      ['table_beforeTax', formatCurrency(total)],
+      ['table_afterTax', formatCurrency(net)],
+    ]
+
+    table_cells.forEach(cell => getEl(cell[0]).innerText = `${cell[1]}`)
+    getEl("breakdown").scrollIntoView({behavior: "smooth"})
+  });
+}
